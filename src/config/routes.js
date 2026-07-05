6d3ae4aea92381e7c1a66b07b5f536a399940a2a
vanlinh00 <vanlinh20192019@gmail.com>
2026-07-05 22:49:08 +0700

    live

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,6 +1,7 @@
 const routes = {
     home: '/',
     userInfo: '/user-info', // Add this line
+        live: '/live', // Add this line
 };
 
 export default routes;
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,10 +2,12 @@
 
 import config from '../config';
 import Home from '../pages/Home';
+import Live from '../pages/Live';
 import UserInfo from '../pages/Profile'; // 1. Import your new page component
 const publicRoutes = [
  { path: config.routes.home, component: Home },
  { path: config.routes.userInfo, component: UserInfo }, // Add it to the array
+    { path: config.routes.live, component: Live },
 
 ];
 
